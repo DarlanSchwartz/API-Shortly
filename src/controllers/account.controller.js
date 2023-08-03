7e38a9698d4c1e29dfb432fc7eba39bc0e73067f
Schwartz <darlanswtz@gmail.com>
2023-08-03 13:10:03 -0300

    Fix:Removed a comment

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -43,30 +43,6 @@ export async function signIn(req, res) {
         return res.status(500).send('Internal server error');
     }
 }
-/*
-
-{
-  "id": id do usuário,
-    "name": nome do usuário,
-    "visitCount": soma da quantidade de visitas de todos os links do usuário,
-    "shortenedUrls": [
-        {
-            "id": 1,
-            "shortUrl": "...",
-            "url": "...",
-            "visitCount": soma da quantidade de visitas do link
-        },
-        {
-            "id": 2,
-            "shortUrl": "...",'
-            "url": "...",
-            "visitCount": soma da quantidade de visitas do link
-        }
-    ]
-}
-
-*/
-
 
 export async function getUserInfo(req, res) {
     try {
