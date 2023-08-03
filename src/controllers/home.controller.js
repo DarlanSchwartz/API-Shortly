15880f8083249a6d32891ab5053f1e7c6f69f254
Schwartz <darlanswtz@gmail.com>
2023-08-03 15:28:18 -0300

    Fix: links count

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -3,17 +3,19 @@ import db from '../database/database.connection.js';
 export async function getRanking(req, res) {
     try {
         const query = `
-            SELECT id, name,
-                (SELECT SUM(visitcount) FROM "short_urls" WHERE usuario_id = users.id) AS linksCount,
-                (SELECT COALESCE(SUM(visitcount), 0) FROM "short_urls" WHERE "usuario_id" = users.id) AS visitCount
+            SELECT users.id, users.name,
+                COUNT(short_urls.id) AS linksCount,
+                COALESCE(SUM(short_urls.visitcount), 0) AS visitCount
             FROM users
-            ORDER BY visitcount DESC
-            LIMIT 10;
+            LEFT JOIN short_urls ON users.id = short_urls.usuario_id
+            GROUP BY users.id, users.name
+            ORDER BY visitCount DESC
+            LIMIT 5;
         `;
         const result = await db.query(query);
 
-        const ranking = result.rows.map(url =>{
-           
+        const ranking = result.rows.map(url => {
+
             url.linksCount = url.linkscount;
             url.visitCount = url.visitcount;
             delete url.linkscount;
