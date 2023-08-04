590934c77906c9af1593045f2c09d9b6bfd4a154
Schwartz <darlanswtz@gmail.com>
2023-08-04 15:57:05 -0300

    Refactor: getuserinfo -> getuserinfo2

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -44,35 +44,56 @@ export async function signIn(req, res) {
     }
 }
 
-export async function getUserInfo(req, res) {
+// export async function getUserInfo(req, res) {
+//     try {
+//         const userInfo = await db.query(`SELECT * FROM users WHERE "email"=$1`, [res.locals.user.email]);
+
+//         const foundUserUrls = await db.query(`SELECT * FROM short_urls WHERE owner_id=$1`, [userInfo.rows[0].id]);
+//         let allVisitsCount = 0;
+
+//         const userUrls = foundUserUrls.rows.map(url =>{
+//             allVisitsCount += Number(url.visitCount);
+//             return {
+//                 id:url.id,
+//                 url:url.url,
+//                 shortUrl:url.shorturl,
+//                 visitCount:url.visitCount
+//             };
+//         });
+
+//         const user =
+//         {
+//             id: userInfo.rows[0].id,
+//             name: userInfo.rows[0].name,
+//             visitCount: allVisitsCount,
+//             shortenedUrls: userUrls
+//         };
+
+//         return res.status(200).send(user);
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).send('Internal server error');
+//     }
+// }
+
+export async function getUserInfo2(req, res) {
     try {
-        const userInfo = await db.query(`SELECT * FROM users WHERE "email"=$1`, [res.locals.user.email]);
-
-        const foundUserUrls = await db.query(`SELECT * FROM short_urls WHERE owner_id=$1`, [userInfo.rows[0].id]);
-        let allVisitsCount = 0;
-
-        const userUrls = foundUserUrls.rows.map(url =>{
-            allVisitsCount += Number(url.visitCount);
-            return {
-                id:url.id,
-                url:url.url,
-                shortUrl:url.shorturl,
-                visitCount:url.visitCount
-            };
-        });
-
-        const user =
-        {
-            id: userInfo.rows[0].id,
-            name: userInfo.rows[0].name,
-            visitCount: allVisitsCount,
-            shortenedUrls: userUrls
-        };
-
+        const query = `
+            SELECT u.id, u.name,
+                   COALESCE(SUM(s."visitCount"), 0) AS "visitCount",
+                   array_agg(json_build_object('id', s.id, 'url', s.url, 'shortUrl', s.shorturl, 'visitCount', s."visitCount")) AS "shortenedUrls"
+            FROM users u
+            LEFT JOIN short_urls s ON u.id = s.owner_id
+            WHERE u.email = $1
+            GROUP BY u.id, u.name
+        `;
+        const userInfo = await db.query(query, [res.locals.user.email]);
+        
+        const user = userInfo.rows[0];
+        user.visitCount = user.visitCount || 0;
         return res.status(200).send(user);
     } catch (error) {
         console.log(error);
         return res.status(500).send('Internal server error');
     }
 }
-
