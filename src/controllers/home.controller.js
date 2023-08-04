a882683e3b38decb0aca663287716c2580b6b867
Schwartz <darlanswtz@gmail.com>
2023-08-04 16:02:40 -0300

    Refactor: getRanking

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -4,26 +4,17 @@ export async function getRanking(req, res) {
     try {
         const query = `
             SELECT users.id, users.name,
-                COUNT(short_urls.id) AS linksCount,
-                COALESCE(SUM(short_urls."visitCount"), 0) AS visitCount
+                COUNT(short_urls.id) AS "linksCount",
+                COALESCE(SUM(short_urls."visitCount"), 0) AS "visitCount"
             FROM users
             LEFT JOIN short_urls ON users.id = short_urls.owner_id
             GROUP BY users.id, users.name
-            ORDER BY visitCount DESC
+            ORDER BY "visitCount" DESC
             LIMIT 10;
         `;
         const result = await db.query(query);
 
-        const ranking = result.rows.map(url => {
-
-            url.linksCount = url.linkscount;
-            url.visitCount = url.visitcount;
-            delete url.visitcount;
-            delete url.linkscount;
-            return url;
-        });
-
-        return res.status(200).send(ranking);
+        return res.status(200).send(result.rows);
     } catch (error) {
         console.log(error);
         return res.status(500).send('Internal server error');
