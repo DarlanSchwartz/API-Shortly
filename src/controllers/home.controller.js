0cd705cc2520977a628031200f00bd7a559d8b3e
Schwartz <darlanswtz@gmail.com>
2023-08-04 21:27:21 -0300

    Refactor: home/ranking repository

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,20 +1,9 @@
-import db from '../database/database.connection.js';
+import { getOverallRanking } from '../repository/home.repository.js';
 
 export async function getRanking(req, res) {
     try {
-        const query = `
-            SELECT users.id, users.name,
-                COUNT(short_urls.id) AS "linksCount",
-                COALESCE(SUM(short_urls."visitCount"), 0) AS "visitCount"
-            FROM users
-            LEFT JOIN short_urls ON users.id = short_urls.owner_id
-            GROUP BY users.id, users.name
-            ORDER BY "visitCount" DESC
-            LIMIT 10;
-        `;
-        const result = await db.query(query);
-
-        return res.status(200).send(result.rows);
+        const ranking = await getOverallRanking();
+        return res.status(200).send(ranking);
     } catch (error) {
         console.log(error);
         return res.status(500).send('Internal server error');
---
+++ src/repository/home.repository.js
@@ -0,0 +1,19 @@
+export async function getOverallRanking() {
+    try {
+        const query = `
+            SELECT users.id, users.name,
+                COUNT(short_urls.id) AS "linksCount",
+                COALESCE(SUM(short_urls."visitCount"), 0) AS "visitCount"
+            FROM users
+            LEFT JOIN short_urls ON users.id = short_urls.owner_id
+            GROUP BY users.id, users.name
+            ORDER BY "visitCount" DESC
+            LIMIT 10;
+        `;
+        const result = await db.query(query);
+
+        return result.rows;
+    } catch (error) {
+        return null;
+    }
+}
